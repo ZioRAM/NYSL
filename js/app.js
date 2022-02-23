08ada850dce4b78a79b32d22cdf59f1aa02d57ea
ZioRAM <ezequieladrianibarra@gmail.com>
2022-02-22 22:23:46 -0300

    big changes

--- js/app.js
+++ js/app.js
@@ -11,3 +11,21 @@ window.addEventListener('scroll', () =>{
     }
 })
 
+
+data[0].games.forEach(element => {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+        <td>${element.day}</td>
+        <td>${element.vsName} <br> <a href="${element.location.linkGM}">${element.location.name}</a></td>
+        <td>${element.times}</td>`;
+    document.getElementById("tbody1").appendChild(tr);
+});
+
+data[1].games.forEach(element => {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+        <td>${element.day}</td>
+        <td>${element.vsName} <br> <a href="${element.location.linkGM}">${element.location.name}</a></td>
+        <td>${element.times}</td>`;
+    document.getElementById("tbody2").appendChild(tr);
+});
